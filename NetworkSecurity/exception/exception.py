5e6fac634edd3c01a4517eae0490a7aca375840e
Mo Elbadawy <mohamed.asaad.elbadawy@gmail.com>
2026-03-04 16:57:59 +0200

    Exception handling and logging completed

---
+++ NetworkSecurity/exception/exception.py
@@ -0,0 +1,28 @@
+import sys
+from NetworkSecurity.logging import logger
+class NetworkSecurityException(Exception):
+
+    def __init__(self, error_message, error_details):
+        super().__init__(error_message)
+
+        self.error_message = error_message
+
+        _, _, exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return (
+            f"Error occurred in python script name [{self.file_name}] "
+            f"line number [{self.lineno}] "
+            f"error message [{self.error_message}]"
+        )
+
+if __name__ == "__main__":
+    try:
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("this will not be printed",a)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)    
